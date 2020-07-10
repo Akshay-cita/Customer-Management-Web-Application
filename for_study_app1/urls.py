f2e00094b2d5c478174a12fef901dadce92a1284
Akshay-cita <edakkatakshay@gmail.com>
2020-07-10 23:02:18 +0530

    Created urls for Login and Registration page

--- for_study_app1/urls.py
+++ for_study_app1/urls.py
@@ -3,6 +3,8 @@ from for_study_app1 import views
 
 urlpatterns = [
 path('cmr',views.initial_page,name='cmr'),
+path('login/',views.login_page,name='login'),
+path('register/',views.register_page,name='register'),
 path('cmr/dashboard/',views.dashboard_view,name='dashboard'),
 path('cmr/product/',views.product_view,name='product'),
 path('cmr/customer/<str:pk>/',views.customer_view,name='customer'),
@@ -10,8 +12,4 @@ path('cmr/orderform/<str:pk>/',views.OrderFormView,name='orderform'),
 path('cmr/updateorder/<str:pk>/',views.UpdateOrder,name='orderupdate'),
 path('cmr/createcustomer/',views.CreateCustomer,name='createcustomer'),
 path('cmr/deleteorder/<str:pk>/',views.DeleteOrder,name='deleteorder'),
-
-
-
-
 ]
