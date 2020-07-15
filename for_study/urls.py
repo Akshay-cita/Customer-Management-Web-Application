07082e5bb94e4c09472376605f138f39adcda96a
Akshay-cita <edakkatakshay@gmail.com>
2020-07-15 19:22:14 +0530

    urlpattern for showing static image

--- for_study/urls.py
+++ for_study/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 from for_study_app1 import views
 
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path('',include('for_study_app1.urls')),
 
 ]
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
